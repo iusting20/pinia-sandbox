3fa20b828ede2e3110cdec59b1e40afaacc969f9
Iustin Ghergu <iusting20@gmail.com>
2022-05-11 10:33:44 +0100

    added ref

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="home">
-    <div class="count">0</div>
+    <div class="count">{{ count }}</div>
     <div class="buttons">
       <button>-</button>
       <button>+</button>
     </div>
   </div>
 </template>
+
+<script setup>
+import { ref } from "vue";
+
+const count = ref(0);
+</script>
 
 <style>
 .count {
