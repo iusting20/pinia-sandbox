61bbe2765823c058719e017b4ab7d8fa9b8ef6ea
Iustin Ghergu <iusting20@gmail.com>
2022-05-11 10:26:20 +0100

    Initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <div class="count">0</div>
+    <div class="buttons">
+      <button>-</button>
+      <button>+</button>
+    </div>
+  </div>
+</template>
+
+<style>
+.count {
+  font-size: 60px;
+  margin: 20px;
+}
+
+.buttons button {
+  font-size: 40px;
+  margin: 10px;
+}
+</style>
